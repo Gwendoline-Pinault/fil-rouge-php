dfecf6915a7dce5213d1783a124ebb149ac8a4c8
Gwendoline Pinault <pinaultgwendoline@gmail.com>
2024-05-05 19:07:20 +0200

    WIP/lesson 5 - Functions

--- index.php
+++ index.php
@@ -1,24 +1,23 @@
-<?php
-$recipes = [
-  [
-      'title' => 'Cassoulet',
-      'recipe' => 'Etape 1 : des flageolets !',
-      'author' => 'mickael.andrieu@exemple.com',
-      'is_enabled' => true,
-  ],
-  [
-      'title' => 'Couscous',
-      'recipe' => 'Etape 1 : de la semoule',
-      'author' => 'mickael.andrieu@exemple.com',
-      'is_enabled' => false,
-  ],
-  [
-      'title' => 'Escalope milanaise',
-      'recipe' => 'Etape 1 : prenez une belle escalope',
-      'author' => 'mathieu.nebra@exemple.com',
-      'is_enabled' => true,
-  ],
-];
+<?php $recipes = [
+    [
+        'title' => 'Cassoulet',
+        'recipe' => 'Etape 1 : des flageolets !',
+        'author' => 'mickael.andrieu@exemple.com',
+        'is_enabled' => true,
+    ],
+    [
+        'title' => 'Couscous',
+        'recipe' => 'Etape 1 : de la semoule',
+        'author' => 'mickael.andrieu@exemple.com',
+        'is_enabled' => false,
+    ],
+    [
+        'title' => 'Escalope milanaise',
+        'recipe' => 'Etape 1 : prenez une belle escalope',
+        'author' => 'mathieu.nebra@exemple.com',
+        'is_enabled' => true,
+    ],
+  ];
 ?>
 
 <!DOCTYPE html>
@@ -54,7 +53,7 @@ $recipes = [
 foreach ($recipes as $recipe){
 
   // Je vérifie si la clé 'is_enabled' existe dans l'élément du tableau
-  if (array_key_exists('is_enabled', $recipe)) {
+  if (array_key_exists('is_enabled', $recipe)){
 
     // Si la clé existe et qu'elle vaut 'true', j'affiche les informations de la recette
     if ($recipe['is_enabled']) { ?>
@@ -67,7 +66,47 @@ foreach ($recipes as $recipe){
 }
 ?>
 
+<!-- <div class="box" aria-label="Exemples de manipulations de texte">
+  <h2>Tests d'affichage</h2>
+  <h3>Les fonctions de manipulation du texte :</h3>
+  
+  <ul>
+    <li>Compter la longueur d'une string</li>
+    <?php
+    $stringTest = $recipes[1]['recipe'];
+    $recipeLength = strlen($stringTest);
+    echo 'La phrase suivante comporte ' . $recipeLength . ' caractères : [' . $stringTest . ']';
+    ?>
+    
+    <li>Remplacer du texte</li>
+    <?php 
+    // on dit que qu'on remplace et par quoi. On pourrait chercher un mot à la place.
+    echo str_replace('ca', 'Ca', 'le cassoulet, c\'est très bon'); 
+    ?>
+
+    <li>Formater une string</li>
+    <?php 
+    echo sprintf(
+    '%s par "%s" : %s',
+    $recipe['title'],
+    $recipe['author'],
+    $recipe['recipe']
+    ); 
+    // INFO : [%s] signifie argument chaine de caractère (cf Doc PHP)
+    ?>
+
+    <li>La date</li>
+    <?php 
+    $date = date('d/m/Y');
+    $time = date('H:i'); // 'H' pour la version 24h et 'h' pour 12h ; 'i' pour minute parce que m est déjà pris par month
+
+    echo "Bonjour, nous sommes le " . $date . " et il est " . $time;
+    ?>
+
+  </ul>
+</div> -->
+
+
 
 </body>
-
 </html>
