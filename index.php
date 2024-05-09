bee3df7d80ea240dad64f50981c6611de25a895c
Gwendoline Pinault <pinaultgwendoline@gmail.com>
2024-05-09 17:35:16 +0200

    Functions/Print recipes with foreach on an array

--- index.php
+++ index.php
@@ -69,7 +69,6 @@
 -->
 
 <?php 
-/* 
 // On parcourt le tableau $recipes
 foreach ($recipes as $recipe){
 
@@ -84,45 +83,9 @@ foreach ($recipes as $recipe){
       <?php 
     }
   } 
-} */
+}
+
 ?>
-
-<?php
-
-function isValidRecipe(array $recipe) : bool
-{
-    if (array_key_exists('is_enabled', $recipe)) {
-        $isEnabled = $recipe['is_enabled'];
-    } else {
-        $isEnabled = false;
-    }
-
-    return $isEnabled;
-}
-
-function getRecipes(array $recipes) : array
-{
-    $validRecipes = [];
-
-    foreach($recipes as $recipe) {
-        if (isValidRecipe($recipe)) {
-            $validRecipes[] = $recipe;
-        }
-    }
-
-    return $validRecipes;
-}
-
-function displayAuthor(string $authorEmail, array $users): string
-{
-    foreach ($users as $user) {
-        if ($authorEmail === $user['email']) {
-            return $user['full_name'] . '(' . $user['age'] . ' ans)';
-        }
-    }
-}
-?>
-
 
 <!-- <div class="box" aria-label="Exemples de manipulations de texte">
   <h2>Tests d'affichage</h2>
@@ -163,8 +126,6 @@ function displayAuthor(string $authorEmail, array $users): string
 
   </ul>
 </div> -->
-
-
 
 </body>
 </html>
