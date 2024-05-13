1d1b57d85c793f7c2f94c72a3c99bafda038808e
Gwendoline Pinault <pinaultgwendoline@gmail.com>
2024-05-13 12:19:51 +0200

    Organiser les pages d'un site/Création d'un composant header

---
+++ header.php
@@ -0,0 +1,9 @@
+<!-- Menu de navigation type en PHP placé dans un composant à part -->
+<nav id="menu">
+  <h3>Titre menu</h3>
+  <ul>
+    <li><a href="page1.html">Lien</a></li>
+    <li><a href="page2.html">Lien</a></li>
+    <li><a href="page3.html">Lien</a></li>
+  </ul>
+</nav>
