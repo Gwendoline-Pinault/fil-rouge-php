f4ae95f39206828c804c81e6e09c2bf21ad45d44
Gwendoline Pinault <pinaultgwendoline@gmail.com>
2024-05-14 12:02:27 +0200

    création du formulaire de contact (version miniale)

--- contact.php
+++ contact.php
@@ -9,5 +9,17 @@
 <body>
   <?php require_once(__DIR__ . '/header.php'); ?>
   <h1>Contact</h1>
+  <form action="submit" class="container form">
+    <div class="form-subcontainer">
+      <label for="email" class="form-label">Email</label>
+      <input type="email" class="form-control" id="email" name="email" aria-describedby="email-help">
+      <div id="email-help" class="form-text">Nous ne revendrons pas votre email.</div>
+    </div>
+    <div class="form-subcontainer">
+      <label for="message" class="form-label">Votre message</label>
+      <textarea name="form-control" id="message" name="message" placeholder="Exprimez-vous"></textarea>
+    </div>
+    <button type="submit" class="button">Envoyer</button>
+  </form>
 </body>
 </html>
