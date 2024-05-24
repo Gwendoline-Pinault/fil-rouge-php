d7480907153f38840200d18e19a248a0e82515dd
Gwendoline Pinault <pinaultgwendoline@gmail.com>
2024-05-24 10:33:40 +0200

    ajout d'une image au formulaire et vérifications avant stockage

--- contact.php
+++ contact.php
@@ -9,7 +9,7 @@
 <body>
   <?php require_once(__DIR__ . '/header.php'); ?>
   <h1>Contact</h1>
-  <form action="submit_contact.php" method="POST" class="container form">
+  <form action="submit_contact.php" method="POST" enctype="multipart/form-data" class="container form">
     <div class="form-subcontainer">
       <label for="email" class="form-label">Email</label>
       <input type="email" class="form-control" id="email" name="email" aria-describedby="email-help">
@@ -18,6 +18,13 @@
     <div class="form-subcontainer">
       <label for="message" class="form-label">Votre message</label>
       <textarea class="form-control" id="message" name="message" placeholder="Exprimez-vous"></textarea>
+    </div>
+    <div class="form-subcontainer">
+      <div class="flex-row">
+        <label for="image" class="form-label">Image</label>
+        <p class="form-text">&nbsp; (1Mo max)</p>
+      </div>
+      <input type="file" class="form-control" id="image" name="image"/>
     </div>
     <button type="submit" class="button">Envoyer</button>
   </form>
--- submit_contact.php
+++ submit_contact.php
@@ -12,6 +12,38 @@
         // Arrête l'exécution de ce fichier par PHP
         return;
     }
+
+    $uploaded = false;
+    // Variable concernant le fichier transmis (tableau)
+    // Vérification si fichier transmis via formulaire
+    if (isset($_FILES['image']) && $_FILES['image']['error'] == 0){
+        if ($_FILES['image']['size'] > 1000000){
+            echo('Le fichier est trop lourd');
+            return;
+        }
+
+        // Vérification de l'extension du fichier
+        $fileInfo = pathinfo($_FILES['image']['name']);
+        $extension = $fileInfo['extension'];
+        $allowedExtensions = ['jpg', 'jpeg', 'gif', 'png'];
+
+        if (!in_array($extension, $allowedExtensions)) {
+            echo('Le format du fichier est incorrect.');
+            return;
+        }
+
+        // Vérification de l'existence du dossier de stockage
+        $path = __DIR__ . '/uploads/';
+
+        if (!is_dir($path)) {
+            echo('Le dossier de destination n\'existe pas.');
+            return;
+        }
+
+        //Validation de l'upload
+        move_uploaded_file($_FILES['image']['tmp_name'], $path . basename($_FILES['image']['name']));
+        $uploaded = true;
+    }
 ?>
 
 <!DOCTYPE html>
@@ -31,6 +63,10 @@
             <h4 class="card-title">Rappel de vos informations :</h4>
             <p class="card-text"><b>Email</b> : <?php echo $postData['email']; ?></p>
             <p class="card-text"><b>Message</b> : <?php echo strip_tags($postData['message']); ?></p>
+
+            <?php if ($uploaded) : ?>
+                <p>L'envoi a bien été effectué !</p>
+            <?php endif; ?>
         </div>
     </div>
 </body>
